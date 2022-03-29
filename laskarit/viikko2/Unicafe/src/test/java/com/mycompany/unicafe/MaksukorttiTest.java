55a31d5412e0b05dd3a7ce2af4f1db097a23a3d4
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2022-03-29 20:28:23 +0300

    lisää .gitignore ja kirjoita Maksukortti-luokalle testit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,36 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+
+	@Test
+	public void kortinSaldoAlussaOikein() {
+		assertEquals(10, kortti.saldo());
+	}
+
+	@Test
+	public void rahanLataaminenKasvattaaSaldoa() {
+		kortti.lataaRahaa(1);
+		
+		assertEquals(11, kortti.saldo());
+	}
+
+	@Test
+	public void saldoVaheneeKunRahaaTarpeeksi() {
+		kortti.otaRahaa(1);
+		
+		assertEquals(9, kortti.saldo());
+	}
+
+	@Test
+	public void saldoEiMuutuKunRahaaEiRiittavasti() {
+		kortti.otaRahaa(11);
+
+		assertEquals(10, kortti.saldo());
+	}
+
+	@Test
+	public void otaRahaaPalauttaaOikeanArvon() {
+		assertTrue(kortti.otaRahaa(1));
+		assertFalse(kortti.otaRahaa(10));
+	}
 }
